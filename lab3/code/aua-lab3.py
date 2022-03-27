7283e9501946e59d4b6bdf9857bcd24438adbec9
raiL0ng <19nik.nikitov.97@mail.ru>
2022-03-27 19:03:22 +0100

    Вроде как всё:)

--- lab3/code/aua-lab3.py
+++ lab3/code/aua-lab3.py
@@ -1,208 +1,6 @@
 import numpy as np
 
 
-# Построение объединение бинарных отношений
-def get_union_bin_rel(br1, br2=[]):
-  if br2 == []:
-    union = br1.copy()
-    for pair in br1:
-      if pair not in union:
-        union.append(pair)
-    print('Answer:')
-    print_binary_relation(union, len(union))
-  else:
-    union = br1.copy()
-    for pair in br2:
-      if pair not in union:
-        union.append(pair)
-    print('Answer:')
-    print_binary_relation(union, len(union))
-
-
-# Построение пересечения бинарных отношений
-def get_intersection_bin_rel(br1, br2=[]):
-  if br2 == []:
-    intersec = []
-    for x1, y1 in br1:
-      for x2, y2 in br1:
-        if x1 == x2 and y1 == y2:
-          intersec.append((x1, y1))
-    print('Answer:')
-    print_binary_relation(intersec, len(intersec))
-  else:
-    intersec = []
-    for x1, y1 in br1:
-      for x2, y2 in br2:
-        if x1 == x2 and y1 == y2:
-          intersec.append((x1, y1))
-    print('Answer:')
-    print_binary_relation(intersec, len(intersec))
-
-
-# Построение обратного бинарного отношения
-def get_reverse_bin_rel(br):
-  rev_br = []
-  for x, y in br:
-    rev_br.append((y, x))
-  print('Answer:')
-  print_binary_relation(rev_br, len(rev_br))
-
-
-# Построение композиции бинарных отношений
-def get_composition_bin_rel(br1, br2=[]):
-  if br2 == []:
-    comp_br = []
-    for x1, y1 in br1:
-      for x2, y2 in br1:
-        if y1 == x2:
-          comp_br.append((x1, y2))
-    print('Answer:')
-    print_binary_relation(comp_br, len(comp_br))
-  else:
-    comp_br = []
-    for x1, y1 in br1:
-      for x2, y2 in br2:
-        if y1 == x2:
-          comp_br.append((x1, y2))
-    print('Answer:')
-    print_binary_relation(comp_br, len(comp_br))
-
-
-# Выполнение операции сложения матриц
-def get_addition_operation(a, n):
-  print('Enter values of another matrix')
-  b = []
-  for i in range(n):
-    tmp = input().split()
-    for j in range(len(tmp)):
-      try:
-        tmp[j] = float(tmp[j])
-      except:
-        k = 0
-      else:
-        tmp[j] = float(tmp[j])
-    b.append(tmp)
-  c = []
-  for i in range(n):
-    tmp = []
-    for j in range(len(a[i])):
-      tmp.append(a[i][j] + b[i][j])
-    c.append(tmp)
-  
-  return c
-
-
-# Выполнение операции вычитания матриц
-def get_subtraction_operation(a, n):
-  print('Enter values of another matrix')
-  b = []
-  for i in range(n):
-    tmp = input().split()
-    for j in range(len(tmp)):
-      try:
-        tmp[j] = float(tmp[j])
-      except:
-        k = 0
-      else:
-        tmp[j] = float(tmp[j])
-    b.append(tmp)
-  c = []
-  for i in range(n):
-    tmp = []
-    for j in range(len(a[i])):
-      tmp.append(a[i][j] - b[i][j])
-    c.append(tmp)
-  
-  return c
-
-
-# Выполнение операции умножение матрицы на число
-def get_multiplication_matrix_on_number(a):
-  print('Enter number:')
-  num = float(input())
-  c = []
-  for i in range(len(a)):
-    tmp = []
-    for j in range(len(a[i])):
-      tmp.append(a[i][j] * num)
-    c.append(tmp)
-
-  return c
-
-
-# Выполнение операции умножения матриц
-def get_multiplication_operation(a):
-  print('Enter the number of rows')
-  n = int(input())
-  print('Enter the number of columns')
-  m = int(input())
-  print('Enter values of another matrix')
-  b = []
-  for i in range(n):
-    tmp = input().split()
-    for j in range(m):
-      try:
-        tmp[j] = float(tmp[j])
-      except:
-        k = 0
-      else:
-        tmp[j] = float(tmp[j])
-    b.append(tmp)
-  c = []
-  for i in range(m):
-    tmp = []
-    for j in range(m):
-      sum = 0
-      for k in range(n):
-        sum += a[i][k] * b[k][j]
-      tmp.append(sum)
-    c.append(tmp)
-  
-  return c
-
-
-# Выполнение операции транспонирования матрицы
-def get_transpose_operation(a):
-  c = []
-  for i in range(len(a)):
-    tmp = []
-    for j in range(len(a[i])):
-      tmp.append(a[j][i])
-    c.append(tmp)
-  
-  return c
-
-
-# Нахождение обратной матрицы
-def get_inverse_matrix(a):
-  a = np.array(a)
-  return np.linalg.inv(a)
-
-
-# Вывод матрицы
-def print_matrix(a, n):
-    cnt = 0
-    print('Your matrix:')
-    for i in a:
-        if (cnt < n):
-            print(f'{i}' + ' ')
-        else:
-            print(f'{i}' + '\n')
-            cnt = 0
-        cnt += 1
-
-
-# Вывод бинарного отношения
-def print_binary_relation(br, n):
-    print('{ ', end='')
-    for i in range(n):
-        if i == n - 1:
-            print('(' + str(br[i][0]) + ', ' + str(br[i][1]), end=')')
-        else:
-            print('(' + str(br[i][0]) + ', ' + str(br[i][1]), end='), ')
-    print(' }')
-
-
 # Проверка операции на идемпотентность
 def check_idempotence(set_list, a):
   is_idempotent = True
@@ -287,6 +85,221 @@ def check_distributivity(set_list, a):
     print('Binary operation is not distributive')
 
 
+# Построение объединение бинарных отношений
+def get_union_bin_rel(a, b, n):
+  c = []
+  for i in range(n):
+    tmp = []
+    for j in range(len(a[i])):
+      if a[i][j] and b[i][j]:
+        tmp.append(1)
+      else:
+        tmp.append(a[i][j] + b[i][j])
+    c.append(tmp)
+  print('*******************************************')
+  print_matrix(c, n)
+  go_to_set(c, n)
+  print('*******************************************')
+
+
+# Построение пересечения бинарных отношений
+def get_intersection_bin_rel(a, b, n):
+  c = []
+  for i in range(n):
+    tmp = []
+    for j in range(len(a[i])):
+      tmp.append(a[i][j] * b[i][j])
+    c.append(tmp)
+  print('*******************************************')
+  print_matrix(c, n)
+  go_to_set(c, n)
+  print('*******************************************')
+
+
+# Построение дополнения бинарного отношения
+def get_addition_bin_rel(a, n):
+  c = []
+  for i in range(n):
+    tmp = []
+    for j in range(n):
+      tmp.append(1 - a[i][j])
+    c.append(tmp)
+  print('*******************************************')
+  print_matrix(c, n)
+  go_to_set(c, n)
+  print('*******************************************')
+
+
+# Построение обратного бинарного отношения
+def get_reverse_bin_rel(a, n):
+  c = np.array(a)
+  c = c.transpose()
+  print('*******************************************')
+  print_matrix(c, n)
+  go_to_set(c, n)
+  print('*******************************************')
+
+
+# Построение композиции бинарных отношений
+def get_composition_bin_rel(a, b, n):
+  c = []
+  for i in range(n):
+    tmp = []
+    for j in range(n):
+      sum = 0
+      for k in range(n):
+        sum += a[i][k] * b[k][j]
+        if sum > 1:
+          sum = 1
+      tmp.append(sum)
+    c.append(tmp)
+  print('*******************************************')
+  print_matrix(c, n)
+  go_to_set(c, n)
+  print('*******************************************')
+
+# Выполнение операции сложения матриц
+def get_addition_operation(a, n):
+  print('Enter values of another matrix')
+  b = []
+  for i in range(n):
+    tmp = input().split()
+    for j in range(len(tmp)):
+      try:
+        tmp[j] = float(tmp[j])
+      except:
+        k = 0
+      else:
+        tmp[j] = float(tmp[j])
+    b.append(tmp)
+  c = []
+  for i in range(n):
+    tmp = []
+    for j in range(len(a[i])):
+      tmp.append(a[i][j] + b[i][j])
+    c.append(tmp)
+  return c
+
+
+# Выполнение операции вычитания матриц
+def get_subtraction_operation(a, n):
+  print('Enter values of another matrix')
+  b = []
+  for i in range(n):
+    tmp = input().split()
+    for j in range(len(tmp)):
+      try:
+        tmp[j] = float(tmp[j])
+      except:
+        k = 0
+      else:
+        tmp[j] = float(tmp[j])
+    b.append(tmp)
+  c = []
+  for i in range(n):
+    tmp = []
+    for j in range(len(a[i])):
+      tmp.append(a[i][j] - b[i][j])
+    c.append(tmp)
+  return c
+
+
+# Выполнение операции умножение матрицы на число
+def get_multiplication_matrix_on_number(a):
+  print('Enter number:')
+  num = float(input())
+  c = []
+  for i in range(len(a)):
+    tmp = []
+    for j in range(len(a[i])):
+      tmp.append(a[i][j] * num)
+    c.append(tmp)
+  return c
+
+
+# Выполнение операции умножения матриц
+def get_multiplication_operation(a):
+  print('Enter the number of rows')
+  n = int(input())
+  print('Enter the number of columns')
+  m = int(input())
+  print('Enter values of another matrix')
+  b = []
+  for i in range(n):
+    tmp = input().split()
+    for j in range(m):
+      try:
+        tmp[j] = float(tmp[j])
+      except:
+        k = 0
+      else:
+        tmp[j] = float(tmp[j])
+    b.append(tmp)
+  c = []
+  for i in range(len(a)):
+    tmp = []
+    for j in range(m):
+      sum = 0
+      for k in range(n):
+        sum += a[i][k] * b[k][j]
+      tmp.append(sum)
+    c.append(tmp)
+  return c
+
+
+# Выполнение операции транспонирования матрицы
+def get_transpose_operation(a):
+  c = []
+  for i in range(len(a)):
+    tmp = []
+    for j in range(len(a[i])):
+      tmp.append(a[j][i])
+    c.append(tmp)
+  return c
+
+
+# Нахождение обратной матрицы
+def get_inverse_matrix(a):
+  a = np.array(a)
+  return np.linalg.inv(a)
+
+
+# Вывод матрицы
+def print_matrix(a, n):
+    cnt = 0
+    print('Your matrix:')
+    for i in a:
+        if (cnt < n):
+            print(f'{i}' + ' ')
+        else:
+            print(f'{i}' + '\n')
+            cnt = 0
+        cnt += 1
+
+
+# Вывод бинарного отношения
+def print_binary_relation(br, n):
+    print('Your binary relation')
+    print('{ ', end='')
+    for i in range(n):
+        if i == n - 1:
+            print('(' + str(br[i][0] + 1) + ', ' + str(br[i][1] + 1), end=')')
+        else:
+            print('(' + str(br[i][0] + 1) + ', ' + str(br[i][1] + 1), end='), ')
+    print(' }')
+
+
+# Построение бинарного отношения по матрице
+def go_to_set(a, n):
+    s = []
+    for i in range(n):
+        for j in range(n):
+            if a[i][j] == 1:
+                s.append((i, j))
+
+    print_binary_relation(s, len(s))
+
+
 # Проверка свойств (меню)
 def check_properties_mode(set_list, a):
     print_matrix(a, len(a))
@@ -317,96 +330,98 @@ def check_properties_mode(set_list, a):
 
 
 # Проверка свойств бинарных отношений
-def check_all_bin_rel_properties(br1, br2=[]):
-  if br2 == []:
-    print('Your binary relation:')
-    print_binary_relation(br1, len(br1))
+def check_all_bin_rel_properties(a, b=[]):
+  if b == []:
+    print_matrix(a, len(a))
+    go_to_set(a, len(a))
     print('Choose operation:')
     print('Press 1 to get union of binary relation')
     print('Press 2 to get intersection of binary relation')
-    print('Press 3 to get reverse binary relation')
-    print('Press 4 to get composition of binary relation')
+    print('Press 3 to get addition binary relation')
+    print('Press 4 to get reverse binary relation')
+    print('Press 5 to get composition of binary relation')
     bl = input()
     if bl == '1':
-      get_union_bin_rel(br1)
-      check_all_bin_rel_properties(br1)
+      get_union_bin_rel(a, a, len(a))
+      check_all_bin_rel_properties(a)
     elif bl == '2':
-      get_intersection_bin_rel(br1)
-      check_all_bin_rel_properties(br1)
+      get_intersection_bin_rel(a, a, len(a))
+      check_all_bin_rel_properties(a)
     elif bl == '3':      
-      get_reverse_bin_rel(br1)
-      check_all_bin_rel_properties(br1)
-    elif bl == '4':
-      get_composition_bin_rel(br1)
-      check_all_bin_rel_properties(br1)
+      get_addition_bin_rel(a, len(a))
+      check_all_bin_rel_properties(a)
+    elif bl == '4':      
+      get_reverse_bin_rel(a, len(a))
+      check_all_bin_rel_properties(a)
+    elif bl == '5':
+      get_composition_bin_rel(a, a, len(a))
+      check_all_bin_rel_properties(a)
     else:
-      return choose_mode()
+      print('Exit...')
   else:
-    print('Your binary relations:')
-    print('First:', end='')
-    print_binary_relation(br1, len(br1))
-    print('Second', end='')
-    print_binary_relation(br2, len(br2))
+    print('Your binary relations matrix:')
+    print('First:')
+    print_matrix(a, len(a))
+    go_to_set(a, len(a))
+    print('Second:')
+    print_matrix(b, len(b))
+    go_to_set(a, len(a))
     print('Choose operation:')
     print('Press 1 to get union of binary relations')
     print('Press 2 to get intersection of binary relations')
-    print('Press 3 to get reverse binary relations')
-    print('Press 4 to get composition of binary relations')
+    print('Press 3 to get addition binary relations')
+    print('Press 4 to get reverse binary relations')
+    print('Press 5 to get composition of binary relations')
     bl = input()
     if bl == '1':
-      get_union_bin_rel(br1, br2)
-      check_all_bin_rel_properties(br1, br2)
+      get_union_bin_rel(a, b, len(a))
+      check_all_bin_rel_properties(a, b)
     elif bl == '2':
-      get_intersection_bin_rel(br1, br2)
-      check_all_bin_rel_properties(br1, br2)
+      get_intersection_bin_rel(a, b, len(a))
+      check_all_bin_rel_properties(a, b)
     elif bl == '3':
       print('First:', end='')
-      get_reverse_bin_rel(br1)
-      print('Second', end='')
-      get_reverse_bin_rel(br2)
-      check_all_bin_rel_properties(br1, br2)
+      get_addition_bin_rel(a, len(a))
+      print('Second:', end='')
+      get_addition_bin_rel(b, len(b))
+      check_all_bin_rel_properties(a, b)
     elif bl == '4':
-      get_composition_bin_rel(br1, br2)
-      check_all_bin_rel_properties(br1, br2)
+      print('First:', end='')
+      get_reverse_bin_rel(a, len(a))
+      print('Second:', end='')
+      get_reverse_bin_rel(b, len(b))
+      check_all_bin_rel_properties(a, b)
+    elif bl == '5':
+      get_composition_bin_rel(a, b, len(a))
+      check_all_bin_rel_properties(a, b)
     else:
       print('Exit...')
-      return choose_mode()
 
 
 # Построение бинарных отношений
 def construction_of_binary_relation():
-    print('Enter numbers of binary relation:')
-    s = input()
-    tmp = [i for i in s.split(' ')]
-    if len(tmp) % 2 != 0:
-      print('Incorrect input!')
-      return choose_mode()
-    else:
-      br1 = []
-      k = 0
-      for i in range(1, len(tmp)):
-        if i % 2 != 0:
-          br1.append((tmp[i - 1], tmp[i]))
-          k += 1
-      print('Do you want to enter other binary relation? (0 - no, 1 - yes):')
-      bl = input()
-      print('Enter numbers of binary relation:')
-      if bl == '1':
-        s = input()
-        tmp = [i for i in s.split(' ')]
-        if len(tmp) % 2 != 0:
-          print('Incorrect input!')
-          choose_mode()
-        else:
-          br2 = []
-          k = 0
-          for i in range(1, len(tmp)):
-            if i % 2 != 0:
-              br2.append((tmp[i - 1], tmp[i]))
-              k += 1
-          check_all_bin_rel_properties(br1, br2)
-      elif bl == '0':
-        check_all_bin_rel_properties(br1)
+    print('Enter number of binary relation elements')
+    n = int(input())
+    print('Enter values of binary relation matrix')
+    a = []
+    for i in range(n):
+      tmp = input().split()
+      for j in range(len(tmp)):
+        tmp[j] = int(tmp[j])
+      a.append(tmp)
+    print('Do you want to enter other binary relation? (0 - no, 1 - yes):')
+    bl = input()
+    if bl == '1':
+      print('Enter values of binary relation matrix')
+      b = []
+      for i in range(n):
+        tmp = input().split()
+        for j in range(len(tmp)):
+          tmp[j] = int(tmp[j])
+        b.append(tmp)
+      check_all_bin_rel_properties(a, b)
+    elif bl == '0':
+      check_all_bin_rel_properties(a)
 
     return choose_mode()
 
