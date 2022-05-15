15e1d3951467908939ba6030b33e96401d3b919d
raiL0ng <19nik.nikitov.97@mail.ru>
2022-05-15 01:16:29 +0100

    Some changes

--- lab5/code/aua-lab5.py
+++ lab5/code/aua-lab5.py
@@ -3,6 +3,9 @@ import math
 from itertools import product
 
 
+order = []
+component = []
+
 def print_set(s):
     print('{', end=' ')
     k = 1
@@ -27,96 +30,11 @@ def check_associative(set_list, a):
   return True
 
 
-def create_subsemigroup():
-    print('Enter set values:')
-    s = input()
-    set_list = [i for i in s.split(' ')]
-    print('Enter Cayley table values:')
-    c_tbl = []
-    for i in range(len(set_list)):
-      c_tbl.append([j for j in input().split()])     
-    print('Enter subset values:')
-    s = input()
-    subset_list = [i for i in s.split(' ')]
-    new_subset = subset_list.copy()
-    while True:
-        tmp_set = []
-        for el1 in set_list:
-            for el2 in new_subset:
-                tmp_set.append(c_tbl[set_list.index(el2)][set_list.index(el1)])
-        subsemigroup = set(new_subset).union(set(tmp_set))
-        if (subsemigroup == set(new_subset)):
-            break
-        else:
-            new_subset = list(subsemigroup)
-    
-    subsemigroup = list(subsemigroup)
-    subsemigroup.sort()
-    print('Your subsemigroup:', end='')
-    print_set(subsemigroup, len(subsemigroup))
-    choose_mode()
-
-
-def create_semigroup_via_set():
-    print('Enter semigroup values')
-    s = input()
-    semigroup = [i for i in s.split(' ')]
-    n = len(semigroup)
-    print('Enter transformation set values:')
-    s = input()
-    set_list = [i for i in s.split(' ')]
-    m = len(set_list)    
-    translation_list = []
-    for i in range(m):
-        print(f"Enter transformation values '{set_list[i]}' via elements of semigroup: ")
-        translation = input().split()
-        translation_list.append(translation)    
-
-    combinations = []
-    for i in range(1, m + 1):
-        comb = list(product(''.join([str(elem) for elem in set_list]), repeat=i))
-        combinations += comb
-    
-    ans = {}
-    for comb in combinations:
-        correlation_list = []
-        for i in semigroup:
-            semigroup_elem = i
-            for generator in comb:
-                if semigroup_elem not in semigroup:
-                    semigroup_elem = "*"
-                else:
-                    semigroup_elem = translation_list[set_list.index(generator)] \
-                                                     [semigroup.index(semigroup_elem)]
-            correlation_list.append(semigroup_elem)
-        ans[comb] = correlation_list
-
-    result = {}
-    correlations = {}
-    for key, value in ans.items():
-        if not any(np.array_equal(value, i) for i in result.values()):
-            result[key] = value
-        else:
-            for k, v in result.items():
-                if np.array_equal(v, value):
-                    correlations[key] = k
-
-    print("Your presentations: ")
-    for key, value in result.items():
-        print(key, ":\n", value)
-
-    print("Your corelations: ")
-    for key, value in correlations.items():
-        print(key, "->", value)
-
-    choose_mode()
-
-
 def get_right_ideal(x, set_list, c_tbl):
     right_ideal = set()
     indx = set_list.index(x)
     for el in c_tbl[indx]:
-        right_ideal = right_ideal.union(el)
+        right_ideal.add(el)
     return right_ideal
 
 
@@ -124,61 +42,164 @@ def get_left_ideal(x, set_list, c_tbl):
   left_ideal = set()
   indx = set_list.index(x)
   for i in range(len(c_tbl)):
-    left_ideal = left_ideal.union(c_tbl[i][indx])
+    left_ideal.add(c_tbl[i][indx])
   return left_ideal
+
+
+def dfs(gr, visited, v):
+    visited[v] = True
+    for i in range(len(gr)):
+        u = i
+        if (not(visited[u]) and gr[v][u]):
+            dfs(gr, visited, u)
+    order.append(v)
+
+
+def dfs1(t_gr, visited, v):
+    visited[v] = True
+    component.append(v)
+    for i in range(len(t_gr)):
+        u = i
+        if (not(visited[u]) and t_gr[v][u]):
+            dfs1(t_gr, visited, u)
+
+
+def print_egg_boxes(semigroup, egg_box):
+    print('Your egg-box-diagram:')
+    print('{* 1 }')
+    for box in egg_box:
+        print('{*', end=' ')
+        for i in range(len(box)):
+            print(semigroup[box[i]], end=' ')
+        print('}')
+
+
+def get_egg_boxes(semigroup, d):
+    n = len(d)
+    visited = [False for _ in range(n)]
+    for i in range(n):
+        if (not(visited[i])):
+            dfs(d, visited, i)
+    visited = [False for _ in range(n)]
+    egg_box = []
+    for i in range(n):
+        v = order[n - 1 - i]
+        if (not(visited[v])):
+            dfs1(d.T, visited, v)
+            egg_box.append(component.copy())
+            component.clear()
+    order.clear()
+    return egg_box
+
+
+def create_Grin_relation(semigroup, right_ideals_dict, left_ideals_dict):
+    r = []
+    l = []
+    for el1 in semigroup:
+        tmp_a = []
+        tmp_b = []
+        for el2 in semigroup:
+            if right_ideals_dict[el1] == right_ideals_dict[el2]:
+                tmp_a.append(1)
+            else:
+                tmp_a.append(0)
+            if left_ideals_dict[el1] == left_ideals_dict[el2]:
+                tmp_b.append(1)
+            else:
+                tmp_b.append(0)
+        r.append(tmp_a)
+        l.append(tmp_b)
+    n = len(semigroup)
+    # d = []
+    # for i in range(n):
+    #     tmp_a = []
+    #     for j in range(n):
+            # if r[i][j] == l[i][j] == 1:
+            #     tmp_a.append(1)
+            # else:
+            #     tmp_a.append(r[i][j] + l[i][j])
+    #     d.append(tmp_a)
+    d = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            if r[i][j] == l[i][j] == 1:
+                d[i][j] = 1
+            else:
+                d[i][j] = r[i][j] + l[i][j]
+            
+    print('Your Grin\'s relation:')
+    for el in d:
+        print(el)
+    
+    print('Would you like to get egg-box-diagram? (1 - "yes")')
+    bl = input()
+    if bl == '1':
+        egg_box_list = get_egg_boxes(semigroup, d)
+
+    print_egg_boxes(semigroup, egg_box_list)
+
+def get_and_set_ideals(semigroup, c_tbl):
+    right_ideals_dict = {}
+    left_ideals_dict = {}
+    for el in semigroup:
+        print('-----------------------------------------')
+        print(f'Right ideal ({el}]:', end=' ')
+        right_ideals_dict[el] = get_right_ideal(el, semigroup, c_tbl)
+        tmp_set = list(right_ideals_dict[el])
+        tmp_set.sort()
+        print_set(tmp_set)
+        print(f'Left ideal [{el}):', end=' ')
+        left_ideals_dict[el] = get_left_ideal(el, semigroup, c_tbl)
+        tmp_set = list(left_ideals_dict[el])
+        tmp_set.sort()
+        print_set(tmp_set)
+        print(f'Ideal [{el}]:', end=' ')
+        tmp_set = list(left_ideals_dict[el].union(right_ideals_dict[el]))
+        tmp_set.sort()
+        print_set(tmp_set)
+    print('-----------------------------------------')
+    return right_ideals_dict, left_ideals_dict
 
 
 def create_ideals():
     print('Enter set values:')
     s = input()
-    set_list = [i for i in s.split(' ')]
-    n = len(set_list)
+    semigroup = [i for i in s.split(' ')]
+    n = len(semigroup)
     print('Enter Cayley table values:')
     c_tbl = []
     for i in range(n):
       c_tbl.append([j for j in input().split()])
     
-    if check_associative(set_list, c_tbl) == False:
+    if check_associative(semigroup, c_tbl) == False:
         print('Cayley table isn\'t associative!')
         return choose_mode()
     
-    for el in set_list:
-        print('-----------------------------------------')
-        print(f'Right ideal ({el}]:', end=' ')
-        right_ideal = get_right_ideal(el, set_list, c_tbl)
-        tmp_set = list(right_ideal)
-        tmp_set.sort()
-        print_set(tmp_set)
-        print(f'Left ideal [{el}):', end=' ')
-        left_ideal = get_left_ideal(el, set_list, c_tbl)
-        tmp_set = list(left_ideal)
-        tmp_set.sort()
-        print_set(tmp_set)
-        print(f'Ideal [{el}]:', end=' ')
-        tmp_set = list(left_ideal.union(right_ideal))
-        tmp_set.sort()
-        print_set(tmp_set)
-    print('-----------------------------------------')
+    right_ideals_dict, left_ideals_dict = get_and_set_ideals(semigroup, c_tbl)
     
+    print('Would you to create Grin\'s relation? (1 - "yes")')
+    bl = input()
+    if bl == '1':
+        create_Grin_relation(semigroup, right_ideals_dict, left_ideals_dict)
     return choose_mode()
 
 
-def create_table(set_list, n, presentation):
-  a = []
-  for i in range(n):
-    tmp_a = []
-    for j in range(n):
-      new_word = set_list[i] + set_list[j]
-      while True:
-          tmp = str(new_word)
-          for key, val in presentation.items():
-              if key in new_word:
-                  new_word = new_word.replace(key, val)
-          if tmp == new_word:
-              break
-      tmp_a.append(new_word)
-    a.append(tmp_a)
-  return a
+def create_table(semigroup, n, presentation):
+    a = []
+    for i in range(n):
+        tmp_a = []
+        for j in range(n):
+            new_word = semigroup[i] + semigroup[j]
+            while True:
+                tmp = str(new_word)
+                for key, val in presentation.items():
+                    if key in new_word:
+                        new_word = new_word.replace(key, val)
+                if tmp == new_word:
+                    break
+            tmp_a.append(new_word)
+        a.append(tmp_a)
+    return a
 
 
 def create_semigroup_via_subset():
@@ -201,7 +222,7 @@ def create_semigroup_via_subset():
             for el2 in semigroup:
                 new_word = el1 + el2
                 while True:
-                    tmp = str(new_word)
+                    tmp = new_word
                     for key, val in presentation.items():
                         if key in new_word:
                             new_word = new_word.replace(key, val)
@@ -221,24 +242,29 @@ def create_semigroup_via_subset():
     print('Cayley table:')
     for line in tbl:
         print(line)
+    
+    right_ideals_dict, left_ideals_dict = get_and_set_ideals(semigroup, tbl)
+    
+    print('Would you to create Grin\'s relation? (1 - "yes")')
+    bl = input()
+    if bl == '1':
+        create_Grin_relation(semigroup, right_ideals_dict, left_ideals_dict)
+    
     choose_mode()
 
 
 # Главное меню
 def choose_mode():
     print('Choose mode:')
-    print('Press 1 to create ideals of semigroup')
-    print('Press 2 to create Grin\'s relations')
-    print('Print 3 to create Grin\'s relations via subset')
-    print('Press 4 to exit')
+    print('Press 1 to create Grin\'s relations')
+    print('Press 2 to create Grin\'s relations via subset')
+    print('Press 3 to exit')
     bl = input()
     if bl == '1':
         create_ideals()
     elif bl == '2':
-        print('task 2')
-    elif bl == '3':
         create_semigroup_via_subset()
-    elif bl == '4':
+    elif bl == '3':
         return
     else:
         print('Incorrect output')
